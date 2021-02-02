1800f799d0222d569a35c5bf69e15d478fe6577c
popmonac <78390497+popmonac@users.noreply.github.com>
2021-02-02 01:11:08 +0000

    Add files via upload

---
+++ DbConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace PolymorphismAssignment
+{
+    public abstract class DbConnection
+    {
+        private readonly string _connectionString; private readonly string _timeout;
+        protected DbConnection(string connectionString) { if (String.IsNullOrWhiteSpace(connectionString)) throw new ArgumentException("The connection string cannot be null,whitespace or empty"); _connectionString = connectionString; }
+        public abstract void Open(); public abstract void Close();
+    }
+}
---
+++ SqlConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace PolymorphismAssignment
+{
+    public abstract class SqlConnection : DbConnection
+    {
+        public SqlConnection(string connectionString) : base(connectionString)
+        {
+        }
+        public override void Open() { Console.WriteLine("SqlConnection is open."); }
+        public override void Close() { Console.WriteLine("SqlConnection is close."); }
+    }
+}

d576cf5f6ebe9800e220eb855969812e7cfec143
popmonac <78390497+popmonac@users.noreply.github.com>
2021-02-02 01:08:32 +0000

    Add files via upload

---
+++ OracleConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace PolymorphismAssignment
+{
+    public abstract class OracleConnection : DbConnection
+    {
+        public OracleConnection(string connectionString) : base(connectionString)
+        {
+        }
+        public override void Open() { Console.WriteLine("OracleConnection is open."); }
+        public override void Close() { Console.WriteLine("OracleConnection is close."); }
+    }
+}
